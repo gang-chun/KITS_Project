b5efbb1e6c29ce8a35fa6a6787873b53130d51f3
Lucy Chung <lchung@unomaha.edu>
2021-02-24 17:16:58 -0600

    Added base.html, home.html, styles.css connected to urls and views

--- KITS/views.py
+++ KITS/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import *
+# from .forms import *
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 
-# Create your views here.
+now = timezone.now()
+def index(request):
+    return render(request, 'registration/login.html')
+
+def login(request):
+   return render(request, 'registration/login.html',
+                 {'kits': login})
+
+def home(request):
+   return render(request, 'kits/home.html')
