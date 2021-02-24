565b97c7236f5157f719c464aa27af7115769a5b
steven <spress61@live.com>
2021-02-24 17:20:47 -0600

    pk test

--- KITS/views.py
+++ KITS/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import *
+from .forms import *
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
 
-# Create your views here.
+
+
+#now = timezone.now()
+#def home(request):
+#   return render(request, 'KITS/home.html',
+#                 {'KITS': home})
