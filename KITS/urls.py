9d829ba323de6b26c5a9bf43262ec9abd57b8b6b
gang-chun <60902404+gang-chun@users.noreply.github.com>
2021-02-25 23:36:44 -0600

    Updated URL paths to redirect to home page upon landing, removed redundant login path (already defined in django.auth URLs)

--- KITS/urls.py
+++ KITS/urls.py
@@ -1,15 +1,10 @@
-from django.conf.urls import url
+# from django.conf.urls import url
 from . import views
 from django.urls import path, re_path
 
 app_name = 'KITS'
 name: object
 urlpatterns = [
-    path('', views.index, name='index'),
-    re_path(r'^home/$', views.home, name='home'),
-    path('study/', views.study, name = 'study'),
-    path('login/', views.login, name='login'),
-
-
+    path('', views.home, name='home'),
+    path('study/', views.study, name='study'),
 ]
-
--- KITS_Project/urls.py
+++ KITS_Project/urls.py
@@ -19,10 +19,12 @@ from django.urls import path
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('KITS.urls')),
+    path('KITS/', include('KITS.urls')),
+    path('', RedirectView.as_view(url='KITS/')),
 ]
 
 
