ea4719bbdb9a85880987eeb7d77a32255999f51e
gang-chun <60902404+gang-chun@users.noreply.github.com>
2021-03-01 22:40:23 -0600

    Removed unused import and fixed weak errors

--- KITS/urls.py
+++ KITS/urls.py
@@ -1,11 +1,11 @@
 # from django.conf.urls import url
 from . import views
-from django.urls import path, re_path
+from django.urls import path
 
 app_name = 'KITS'
 name: object
 urlpatterns = [
     path('', views.home, name='home'),
     path('study/', views.study, name='study'),
-    path('create_study/', views.create_study, name = 'create_study'),
+    path('create_study/', views.create_study, name='create_study'),
 ]
