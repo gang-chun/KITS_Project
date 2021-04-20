19314034ca87b31b3991cd133ada85fbf8b469a2
Lucy Chung <lchung@unomaha.edu>
2021-04-20 11:47:39 -0500

    datavisualization.py added

---
+++ KITS/datavisualization.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from .reports import query_active_studies
+
+def visualize_active_studies(queryset):
+    return queryset
+
+
+
+
