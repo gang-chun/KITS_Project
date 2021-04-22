97d31e55f1b0009b24bfd9cb42e47e78c631da98
Lucy Chung <lchung@unomaha.edu>
2021-04-21 19:57:31 -0500

    kit activity updates

--- KITS/datavisualization.py
+++ KITS/datavisualization.py
@@ -22,8 +22,10 @@ def bar_graph_active_studies(file, start, end):
     # Prepare the bar graph
     graph1 = px.bar(data_frame=data, x="Date", y="count", color="Action", orientation="v", barmode='group',
     color_discrete_sequence = ['#222831', '#325288', '#17a2b8'], title="Overall Kit Activity")
+    graph1.update_layout(plot_bgcolor='#dce4e4', autosize=True, margin=dict(l=30, r=30,b=60,t=60,
+        pad=4),)
     graph1.update_xaxes(title_text='Date (month-year)')
-    graph1.update_yaxes(title_text='Number of Kits')
+    graph1.update_yaxes(title_text='Number of Kits', dtick=1)
     # Prepare the bar graph to suit html format
     html_graph = graph1.to_html(full_html=False)
     return html_graph
